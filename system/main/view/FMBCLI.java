78e073b32400565af65eeae8589a60ff4a1f9d0d
Will Hoover <70659810+LordGuacaba@users.noreply.github.com>
2023-05-29 16:43:12 -0400

    vs code stopped yelling at me (for now)

--- system/main/view/FMBCLI.java
+++ system/main/view/FMBCLI.java
@@ -1,5 +1,11 @@
 package system.main.view;
 
+import java.util.List;
+
+import system.main.model.character.Battle;
+import system.main.model.character.Character;
+import system.main.model.character.Team;
+
 /**
  * A {@link UserInterface} for FMB run from the command line.
  * 
@@ -10,6 +16,30 @@ public class FMBCLI implements UserInterface {
     @Override
     public void putMessage(String message) {
         System.out.println(message);
+    }
+
+    @Override
+    public void displayBattle(Battle battle) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'displayBattle'");
+    }
+
+    @Override
+    public void displayTeam(Team team) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'displayTeam'");
+    }
+
+    @Override
+    public void displayCharacters(List<Character> characters) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'displayCharacters'");
+    }
+
+    @Override
+    public void displayCharacter(Character character) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'displayCharacter'");
     }
     
 }
