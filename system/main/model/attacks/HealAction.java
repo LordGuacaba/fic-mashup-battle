4581965cacfb71067a337a13b86abb443f4d5afb
Will Hoover <70659810+LordGuacaba@users.noreply.github.com>
2023-05-30 17:53:49 -0400

    initial healing updates

--- system/main/model/attacks/HealAction.java
+++ system/main/model/attacks/HealAction.java
@@ -1,17 +1,37 @@
 package system.main.model.attacks;
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
 import system.main.model.character.Battle;
+import system.main.model.character.Character;
 
 public class HealAction implements AttackAction {
 
     private int health;
+    private int allies;
+    private boolean self;
 
-    public HealAction(int health) {
+    public HealAction(int health, int numberToHeal, boolean self) {
         this.health = health;
+        this.allies = numberToHeal;
+        this.self = self;
     }
     
     @Override
     public void actOn(Battle battle) {
-        battle.getActive().heal(health);
+        List<Character> toHeal = List.copyOf(battle.getAttackers().getActive());
+        Collections.shuffle(toHeal);
+        toHeal.remove(battle.getActive());
+        for (int i=0; i<allies; i++) {
+            if (i == toHeal.size()) {
+                break;
+            }
+            toHeal.get(i).heal(health);
+        }
+        if (self) {
+            battle.getActive().heal(health);
+        }
     }
 }
